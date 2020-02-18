7e4d2a0fa98bd85cae35acadd528a932b208144f
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-02-17 17:46:29 -0800

    Added basic JSoup commands.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-	    for(var str : args) System.out.println(str);
+        for (var str : args) System.out.println(str);
+
+        var connection = Jsoup.connect("https://en.wikipedia.org/wiki/Web_crawler");
+        try {
+            var doc = connection.get();
+            System.out.println(doc.title());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
