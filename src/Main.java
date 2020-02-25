79a9a2dad821b8cec364a4a40fc1c5bc37cfd920
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-02-24 17:44:47 -0800

    Added comments for basic skeleton

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
-import org.jsoup.Jsoup;
-
 import java.io.IOException;
+
+import static org.jsoup.Jsoup.connect;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        // Read in seed URL
+
+        // Read document
+
+        // Check langauge
+        // Reject if not found or incorrect
+
+        // Enqueue links in document
+
+        // Record count to CSV
+
+        // Dump CSV at end
+
         System.out.println("Hello World");
         for (var str : args) System.out.println(str);
 
-        var connection = Jsoup.connect("https://en.wikipedia.org/wiki/Web_crawler");
+        final var connection = connect("https://en.wikipedia.org/wiki/Web_crawler");
         try {
-            var doc = connection.get();
+            final var doc = connection.get();
             System.out.println(doc.title());
         } catch (IOException e) {
             e.printStackTrace();
