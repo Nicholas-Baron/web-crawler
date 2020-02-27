d3b65617a8dace4d6c605c9b873da85221e9ce6b
Arturo Blandon <arturob1024@gmail.com>
2020-02-27 12:58:06 -0800

    Reads seed url and starts crawling

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
-import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import static org.jsoup.Jsoup.connect;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
+        int numberOfDocs = 1;
         // Read in seed URL
+        Queue<String> frontier = new ArrayDeque<>();
+        frontier.add(args[0]);
+
 
         // Read document
 
-        // Check langauge
+        while(!frontier.isEmpty() && numberOfDocs < 100) {
+            var currentDoc = Jsoup.connect(frontier.poll()).get();
+            Elements urls = currentDoc.select("a[href]");
+            for(Element url : urls){
+                frontier.add(url.absUrl("href"));
+                numberOfDocs++;
+            }
+
+        }
+
+        // Check language
         // Reject if not found or incorrect
 
         // Enqueue links in document
@@ -19,15 +37,17 @@ public class Main {
 
         // Dump CSV at end
 
-        System.out.println("Hello World");
-        for (var str : args) System.out.println(str);
+//        System.out.println("Hello World");
+//        for (var str : args) System.out.println(str);
+//
+//        final var connection = connect("https://en.wikipedia.org/wiki/Web_crawler");
+//        try {
+//            final var doc = connection.get();
+//            System.out.println(doc.title());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
-        final var connection = connect("https://en.wikipedia.org/wiki/Web_crawler");
-        try {
-            final var doc = connection.get();
-            System.out.println(doc.title());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
     }
 }
