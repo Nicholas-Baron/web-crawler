fcb50f812aa53e2aa26d26301f3099e8353536bf
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-02-27 14:01:48 -0800

    Rejected URLs by language

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,21 @@ public class Main {
         int numberOfDocs = 1;
         final int crawlSize = 100;
         // Read in seed URL
+
+        // Seed language
+        String crawlLanguage = null;
         Queue<String> frontier = new ArrayDeque<>();
-        Map<String, Integer> outlinkCount = new HashMap<>(crawlSize);
         frontier.add(args[0]);
+
+        Map<String, Integer> outlinkCount = new HashMap<>(crawlSize);
 
         while (!frontier.isEmpty() && numberOfDocs < crawlSize) {
 
             // Read document
             String currentUrl = frontier.poll();
+            if (currentUrl.isEmpty()) continue;
+
+            System.out.println("Downloading " + currentUrl);
             Document currentDoc = Jsoup.connect(currentUrl).get();
 
             Elements urls = currentDoc.select("a[href]");
@@ -32,15 +39,22 @@ public class Main {
             numberOfDocs++;
 
             // Check language
+            String lang = currentDoc.getElementsByTag("html").attr("lang");
+            if (crawlLanguage == null) {
+                crawlLanguage = lang;
+                System.out.println("Language set to " + crawlLanguage);
+            } else if (!crawlLanguage.equalsIgnoreCase(lang)) {
+                System.out.println("Rejecting " + currentUrl);
+                continue;
+            }
+
+            System.out.println("Accepting " + currentUrl);
             // Reject if not found or incorrect
 
             // Enqueue links in document
             for (Element url : urls) {
                 frontier.add(url.absUrl("href"));
-//                numberOfDocs++;
             }
-            String lang = currentDoc.getElementsByTag("html").attr("lang");
-            System.out.println(lang);
 
             // Record count to CSV
             outlinkCount.put(currentUrl, urls.size());
