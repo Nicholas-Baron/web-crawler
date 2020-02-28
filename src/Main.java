9b56b4b58fc7c8b53ea4bb846f5504c0b4a38435
Howard Montes De Oca <montes.de.oca.howard@gmail.com>
2020-02-27 20:02:20 -0800

    removed php files

--- src/Main.java
+++ src/Main.java
@@ -82,7 +82,7 @@ public class Main {
             String currentUrl = frontier.poll();
             if (currentUrl.contains("#")) currentUrl = currentUrl.substring(0, currentUrl.indexOf("#"));
             if (currentUrl.contains("?")) currentUrl = currentUrl.substring(0, currentUrl.indexOf("?"));
-            if (currentUrl.isEmpty() || currentUrl.endsWith("ogg") || outlinkCount.containsKey(currentUrl)) continue;
+            if (currentUrl.isEmpty() || currentUrl.endsWith("ogg") || currentUrl.endsWith("php")|| outlinkCount.containsKey(currentUrl)) continue;
 
             System.out.println("Downloading " + currentUrl);
             Document currentDoc = Jsoup.connect(currentUrl).get();
